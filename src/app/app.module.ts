58f9dde9908dcaaa5e51415dd1bfc02eff7a9ea2
JohnFabricio11 <51452636+JohnFabricio11@users.noreply.github.com>
2021-04-26 10:29:57 -0500

    App con IONIC de un chatOnline con datos Encriptados utilizando Firebase

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+// Libreria de firebase para la comunicacion con la base de datps..
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, 
+    AngularFireStorageModule
+ ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBYR_M8XwwtXLSNJOjBuW9tA1_rwBZvleU",
+    authDomain: "chatonline-c1ad0.firebaseapp.com",
+    databaseURL: "https://chatonline-c1ad0-default-rtdb.firebaseio.com",
+    projectId: "chatonline-c1ad0",
+    storageBucket: "chatonline-c1ad0.appspot.com",
+    messagingSenderId: "812055306845",
+    appId: "1:812055306845:web:51423e7190d2800a3723b4",
+    measurementId: "G-DJJEM9WWV5"
+  }
+}
